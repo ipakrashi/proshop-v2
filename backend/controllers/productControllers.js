4662e628c443090099fb8333650778061d6ca7ce
ipakrashi <indranil.pakrashi@gmail.com>
2026-07-29 16:53:44 +0530

    Sec 12 001 Create Reviews - Backend

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -88,10 +88,51 @@ const deleteProduct = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc            Create A New Review
+//  route             POST /api/products/:id/reviews
+//  @access         Private
+const createProductReview = asyncHandler(async (req, res) => {
+    const { rating, comment } = req.body
+    const product = await productModel.findById(req.params.id)
+    if (product) {
+        // Check if the Product is already reviewed by the user
+        const alreadyReviewed = await product.reviews.find(
+            (review) => review.user.toString() === req.user._id.toString(),
+        )
+        if (alreadyReviewed) {
+            res.status(400)
+            throw new Error('Product Already Reviewed By You')
+        }
+
+        const review = {
+            name: req.user.name,
+            rating: Number(rating),
+            comment,
+            user: req.user._id,
+        }
+
+        product.reviews.push(review)
+
+        product.numReviews = product.reviews.length
+
+        product.rating =
+            product.reviews.reduce((acc, review) => acc + review.rating, 0) /
+            product.reviews.length
+
+        await product.save()
+
+        res.status(201).json({ message: 'Review Added Successfully' })
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
+})
+
 export {
     getProducts,
     getProductById,
     createProduct,
     updateProduct,
     deleteProduct,
+    createProductReview,
 }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ import {
     getProductById,
     getProducts,
     deleteProduct,
+    createProductReview,
 } from '../controllers/productControllers.js'
 import { protect, admin } from '../middlewares/authMiddleware.js'
 const router = express.Router()
@@ -14,5 +15,6 @@ router.get('/:id', getProductById)
 router.put('/:id', protect, admin, updateProduct)
 router.delete('/:id', protect, admin, deleteProduct)
 router.post('/', protect, admin, createProduct)
+router.post('/:id/reviews', protect, createProductReview)
 
 export default router
