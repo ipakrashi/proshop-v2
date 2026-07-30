be077f40388ff12b0922ddb09b3583c7f90d87fd
ipakrashi <indranil.pakrashi@gmail.com>
2026-07-30 15:09:57 +0530

    Sec 13 002 Deploy to Production

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
     // set Static folder
-    app.use(express.static(path.join(__dirname, 'frontend/bulild')))
+    app.use(express.static(path.join(__dirname, 'frontend/build')))
 
     // any route that is not api will be redirected to index.html
     app.get('{*path}', (req, res) => {
